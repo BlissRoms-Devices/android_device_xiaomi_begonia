5cbb42f14b66b27e38e146356ef81d6093ac816d
thepsambit <psambitk1@gmail.com>
2020-08-08 04:21:41 +0000

    Revert "begonia: Switch to custom POSP apns"
    This reverts commit 6e53bbc9dc3cfddf24231cfad33b5e66dade3454.

--- device.mk
+++ device.mk
@@ -37,10 +37,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
-# Custom APNs
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
